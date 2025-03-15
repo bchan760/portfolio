67febecd7bebb2289f6f9c9a843139130972ade0
Bill Chan <bchan760@gmail.com>
2025-03-14 17:39:51 -0700

    started some code on adding navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = (any) = {
+    return (
+        <nav className="navbar">
+            <a className="navbar-brand" href="#">
+                Navbar
+            </a>
+        </nav>
+    );
+};
+
+export default Navbar;
