fc52b3adf7e346985c9b4052ea760bda30ec3334
Bill Chan <bchan760@gmail.com>
2025-05-01 16:32:52 -0700

    fixed javascript not showing up, added footer, multiple pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Contact from './pages/Contact';
 import MyNavbar from './components/MyNavbar';
+import MyFooter from './components/MyFooter';
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <MyNavbar />
-        <Routes>
+        <header>
+          <a>This is the header</a>
+          <MyNavbar />
+          <Routes>
           <Route path="/contact" element={<Contact />} />
-        </Routes>
+          {/* <Route path="/about_me" element={<About-me />} /> */}
+          </Routes>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+          <MyFooter />
+        </footer>
       </div>
     </Router>
   );
---
+++ frontend/src/components/MyFooter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MyFooter = () => {
+  return (
+    <>
+      {/*This creates a solid line*/}
+      <div style={{ borderTop: "1px solid #ccc", marginTop: "2rem" }}> 
+
+      </div>
+      
+      <footer style={{ padding: "1rem" }}>
+
+        © Copyright 2025. Made by Bill Chan 
+      </footer>
+    </>
+  );
+};
+
+export default MyFooter;
