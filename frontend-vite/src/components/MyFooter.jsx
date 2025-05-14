249ffdec3f91de13293d81e1cf55b6160e19de80
Bill Chan <bchan760@gmail.com>
2025-05-14 15:52:29 -0700

    added google link for typography, fixed social media icons not being in-line

--- frontend-vite/src/components/MyFooter.jsx
+++ frontend-vite/src/components/MyFooter.jsx
@@ -8,7 +8,7 @@ const MyFooter = () => {
       {/* Divider Line */}
       <div className="border-t border-[#C778DD] mt-8"></div>
 
-      <footer className="p-4 text-center text-gray-800 dark:text-white dark:bg-gray-900">
+      <footer className="p-4 text-center text-gray-800 dark:text-white dark:bg-[#2A3132]">
         <div className="flex justify-between items-center flex-wrap gap-4">
           {/* Contact Info (left)*/}
           <div className="flex items-center gap-4">
@@ -16,17 +16,18 @@ const MyFooter = () => {
             <span>Contact me: bchan36@calpoly.edu</span>
           </div>
 
-          {/* Social Media (right)*/}
+          {/* Social Media text(right)*/}
           <div className="flex items-center gap-4">
             <span>Social Media</span>
           </div>
         </div>
+
         <div className="flex justify-between items-center flex-wrap mt-4">
           {/* Web Developer Title (left) */}
           <div>Web Designer and Full Stack Developer</div>
 
           {/* Social Media icons (right) */}
-          <div>
+          <div className="flex gap-4">
             <a href="https://github.com/bchan760" target="_blank" rel="noopener noreferrer">
               <img src={githubIcon} alt="GitHub" className="h-6" />
             </a>
