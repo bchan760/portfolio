7432cc3f133b1facc4b1f57ac33713264d61e15f
Bill Chan <bchan760@gmail.com>
2025-07-22 15:40:48 -0700

    added css for aboutme

--- frontend-vite/src/sections/AboutMe.jsx
+++ frontend-vite/src/sections/AboutMe.jsx
@@ -11,17 +11,25 @@ const ABOUT_CONTENT = {
 const AboutMe = () => {
   return (
     <section id="about-me" className="h1-text-size">
-      <h2 className="text-center">{ABOUT_CONTENT.title}</h2>
+      <h2 className="text-center mb-8">{ABOUT_CONTENT.title}</h2>
 
-      <div className="flex flex-col items-center gap-8 mt-4">
+      <div className="flex flex-col md:flex-row items-center gap-8 mt-4">
         {/* Left side text*/}
-        {ABOUT_CONTENT.paragraphs.map((paragraph, index) => (
-          <p className="h2-text-size" key={index}>{paragraph}</p>
-        ))}
+        <div className="flex-1 md:w-1/2 md:text-left justify-center px-14 py-6">
+          {ABOUT_CONTENT.paragraphs.map((paragraph, index) => (
+            <p className="h2-text-size mb-4" key={index}>{paragraph}</p>
+          ))}
+        </div>
 
         {/* Right side image */}
-        <div className="w-64 h-64 bg-gray-200 rounded-full flex items-center justify-center">
-          <img src={aboutImage} alt="Profile" className="rounded-full w-full h-full object-cover" />
+        <div className='w-full md:w-1/2 flex justify-center'>
+          <div className="w-64 h-64 bg-gray-200 rounded-full flex items-center">
+            <img 
+              src={aboutImage} 
+              alt="Profile" 
+              className="rounded-full w-full h-full shadow-lg" 
+              />
+          </div>
         </div>
       </div>
 
