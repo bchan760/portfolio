333754ced77b10d07a79381f75dacdf84aefcb46
Bill Chan <bchan760@gmail.com>
2025-10-15 21:23:17 -0700

    delete unnecessary files

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,6 +1,5 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './pages/Contact';
 import About from './pages/About'
 import HeaderNav from './components/HeaderNav';
 import Landing from './pages/Landing';
