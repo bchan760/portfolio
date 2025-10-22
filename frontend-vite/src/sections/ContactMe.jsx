828fe6586aee037ea8f6702ec2f38b990554d85a
Bill Chan <bchan760@gmail.com>
2025-10-22 16:53:09 -0700

    added function for handling submissions through contact form. need to test

--- frontend-vite/src/sections/ContactMe.jsx
+++ frontend-vite/src/sections/ContactMe.jsx
@@ -10,6 +10,28 @@ const ContactMe = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
+  const handleChange = (e) => {
+
+  };
+
+  // haven't tested
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    try {
+      setIsSubmitting(false);
+      setIsSubmitted(true);
+    } catch (e) {
+      console.log("this isn't working");
+    }
+    
+    setTimeout(() => {
+      setFormData({ name: '', email: '', message: '' });
+      setIsSubmitted(false);
+    }, 3000);
+  };
+
   return (
     <section id="contact-me" className="min-h-screen flex flex-col items-center justify-center relative">
       <div className="absolute top-8 left-[10%] z-20">
@@ -18,24 +40,27 @@ const ContactMe = () => {
           If you have any questions or just want to say hello, feel free to reach out!
         </p>
       </div>
+      
+      <div>
+        <form>
 
-      <form>
-        <div>
-          <label for="exampleFormControlInput1">Name</label>
-          <textarea></textarea>
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlSelect1">Email</label>
-          <textarea></textarea>
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlSelect2">Message</label>
-          <textarea
-           rows={3}>
-            
-           </textarea>
-        </div>
-      </form>
+          <div>
+            <label for="exampleFormControlInput1">Name</label>
+            <textarea></textarea>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Email</label>
+            <textarea></textarea>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleFormControlSelect2">Message</label>
+            <textarea rows={3}></textarea>
+          </div>
+        </form>
+
+      </div>
       
     </section>
   );
